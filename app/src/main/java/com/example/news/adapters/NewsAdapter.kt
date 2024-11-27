d39e752f001cefd4c7ed367f22e5b786d220f71e
felix <felixdinhit@gmail.com>
2024-11-27 15:07:24 +0700

    news item, news adapter

---
+++ app/src/main/java/com/example/news/adapters/NewsAdapter.kt
@@ -0,0 +1,77 @@
+package com.example.news.adapters
+
+import com.example.news.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.news.models.Article
+
+class NewsAdapter(
+    private val articles: List<Article>
+) : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(
+            oldItem: Article,
+            newItem: Article
+        ): Boolean = oldItem.url == newItem.url
+
+        override fun areContentsTheSame(
+            oldItem: Article,
+            newItem: Article
+        ): Boolean = oldItem == newItem
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview,
+                parent,
+                false,
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: ArticleViewHolder,
+        position: Int
+    ) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this)
+                .load(article.urlToImage)
+                .into(findViewById<ImageView>(R.id.ivImage))
+
+            findViewById<TextView>(R.id.tvTitle).text = article.title
+            findViewById<TextView>(R.id.tvSource).text = article.source.name
+            findViewById<TextView>(R.id.tvPublishDate).text = article.publishedAt
+
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
