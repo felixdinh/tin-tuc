1383ffaa6d536698d171bc5e76d5ac779e30dcb9
felix <felixdinhit@gmail.com>
2024-12-10 11:34:37 +0700

    constant file adding

---
+++ app/src/main/java/com/example/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.util
+
+class Constants {
+   companion object {
+       const val API_KEY = "YOUR_PUBLIC_API_KEY"
+       const val BASE_URL = "https://newsdata.io"
+       const val DELAY_SEARCH_DURATION = 500L
+       const val QUERY_PAGE_SIZE = 10
+   }
+}
